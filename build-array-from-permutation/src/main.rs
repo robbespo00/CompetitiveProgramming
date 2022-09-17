42e074f71d5fdf2acf94cad73e5007a1f6a43a14
Roberto <robyesposito2000@gmail.com>
2022-09-17 17:46:59 +0200

    Added description

--- build-array-from-permutation/src/main.rs
+++ build-array-from-permutation/src/main.rs
@@ -1,3 +1,10 @@
+/*
+* Given a zero-based permutation nums (0-indexed), build an array ans of the same length 
+* where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+*
+* A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+*/
+
 pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
     
     let mut ans = Vec::new();
